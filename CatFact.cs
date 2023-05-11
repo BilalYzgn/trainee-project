3dd4ea97117df042d55c20e8faae3638924e6971
Bilal <m.bilal52@icloud.com>
2023-05-11 11:01:19 +0200

    Aufgae 3

--- CatFact.cs
+++ CatFact.cs
@@ -1,4 +1,11 @@
 public class CatFact
 {
     public string fact { get; set; }
+
+    public void PasteConsole()
+    {
+    Console.WriteLine("---- Fact ----");
+    Console.WriteLine(fact);
+    }
 }
+
--- Program.cs
+++ Program.cs
@@ -2,4 +2,6 @@
 using Flurl.Http;
 
 CatFact catFact1 = await "https://catfact.ninja/fact".GetJsonAsync<CatFact>();
-Console.WriteLine(catFact1.fact);
+{
+catFact1.PasteConsole(); 
+}  
