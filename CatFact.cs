32463b52af97c019a8bd60e4cfc60f819b5c0f12
Bilal <m.bilal52@icloud.com>
2023-05-11 11:12:11 +0200

    Aufgabe 5

--- CatFact.cs
+++ CatFact.cs
@@ -1,11 +1,48 @@
 public class CatFact
 {
-    public string fact { get; set; }
+    
+    public string? fact { get; set; }
+    public string? length { get; set; }
+
+    public int CheckEnter()
+    {
+         Console.WriteLine("Bitte nenne mir eine Zahl zwischen 1 und 10");
+         int zahl = 0;
+    try
+    {
+         zahl = System.Convert.ToInt32(Console.ReadLine());
+      
+      while (zahl<0|zahl >=10){
+        Console.WriteLine("try again");
+      zahl = System.Convert.ToInt32(Console.ReadLine());
+      } 
+    }
+    catch (FormatException)
+    {
+        
+        zahl = CheckEnter();
+    }
+    return zahl;
+    }
+
+    public void ConsoleBeide()
+    {
+    PasteConsole();
+    PasteConsole2();
+    }
 
     public void PasteConsole()
     {
     Console.WriteLine("---- Fact ----");
     Console.WriteLine(fact); 
     }
+
+    public void PasteConsole2()
+    {
+    Console.WriteLine("---- Length ----");
+    Console.WriteLine(length);
+    
+    }
+
 }
 
--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 using Flurl.Http;
 
 CatFact catFact1 = await "https://catfact.ninja/fact".GetJsonAsync<CatFact>();
+CatFact FactLength = await "https://catfact.ninja/fact".GetJsonAsync<CatFact>();
 {
 catFact1.PasteConsole(); 
 } 
+  int loop = catFact1.CheckEnter();
+        for (int i = 0; i < loop; i++)
+      {
+        catFact1.ConsoleBeide(); 
+      }  
